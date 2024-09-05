4570af9d6a723af6ff4a347f7043d88784e6430c
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-09-05 17:10:16 +0700

    V1

--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -2,3 +2,15 @@ export interface TermsAndConditionsProps {
   opened: boolean;
   close: () => void;
 }
+
+export interface FooterProps {
+  year: number;
+  name: string;
+  studentId: string;
+}
+
+export interface CommentReviewProps {
+  name: string;
+  rating: number;
+  review: string;
+}
